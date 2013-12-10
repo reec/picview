c7cf88d83e836c587856080ce41315261ddcbeb9
Ree <ree@unts.se>
2013-12-10 18:50:27 +0100

    First!

---
+++ picview/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for picview project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "picview.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
