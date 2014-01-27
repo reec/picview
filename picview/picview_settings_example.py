ebcd4fb6bbc15d6e4d759414ad1a6142c3687477
Ree <ree@unts.se>
2014-01-27 13:16:41 +0100

    Added an example picview_settings file

---
+++ picview/picview_settings_example.py
@@ -0,0 +1,8 @@
+# Copy this file to picview_settings.py and edit these
+
+# This should be the path to a directory with albums - directories with image files in them.
+PICVIEW_DIR = '/srv/photos'
+
+# You don't need to change anything past here!
+IMAGE_EXTS = ('jpg', 'jpeg', 'png', 'gif', 'bmp')
+VIDEO_EXTS = ('mpg', 'mpeg', '3gp', 'mkv', '.divx', 'xvid')
