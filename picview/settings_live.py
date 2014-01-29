118904179085b49e771976fdf7bddf7ddcd10930
Ree <ree@unts.se>
2014-01-29 20:32:12 +0100

    settings stuffs

---
+++ picview/settings_live.py
@@ -0,0 +1,7 @@
+from picview.settings import *
+# You're going to want a settings_local_live.py too, to specify logging paths and stuff
+
+DEBUG = False
+
+# Set this to something appropriate in your settings_local_live.py
+ALLOWED_HOSTS = '*'
--- picview/wsgi.py
+++ picview/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "picview.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "picview.settings_local_live")
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
